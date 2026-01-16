5c23ee6942df81dcf21b386bc847ff478ba664fb
kiritocode1 <kathawalearyan9@gmail.com>
2026-01-16 10:22:11 +0530

    feat: added shadcn and better-themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { ComponentExample } from "@/components/component-example";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Page() {
-return <ComponentExample />;
+    return (
+        <main className="min-h-screen bg-background transition-colors duration-500">
+            <div className="fixed top-6 right-6 z-50">
+                <ThemeToggle />
+            </div>
+            <ComponentExample />
+        </main>
+    );
 }
